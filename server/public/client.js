d713ac556ec65beed2151c043aaf6dcd599fb576
Andrew Harasymiw <aharasymiw@gmail.com>
2023-11-06 12:31:30 -0800

    update with examples for CRUD, including url params, query params, and body.

--- server/public/client.js
+++ server/public/client.js
@@ -17,6 +17,26 @@ function makeGetCall() {
     );
 }
 
+function makeSearchGetCall(thingId) {
+    let thingStatus = true;
+
+    axios({
+        method: 'GET',
+        url: `/thing/search?thingId=${thingId}&thingStatus=${thingStatus}`,
+    }).then(
+        function (response) {
+            console.log('GET /thing call successful');
+            console.log('response:', response);
+        }
+    ).catch(
+        function (error) {
+            console.log('GET /thing call failed');
+            console.log('error:', error);
+        }
+    );
+}
+
+
 function makePostCall() {
     let newThing = {
         thingDescription: `thing ${Math.floor(Math.random() * 100)}`,
@@ -38,3 +58,38 @@ function makePostCall() {
         }
     )
 }
+
+function makeDeleteCall(thingId) {
+    axios({
+        method: 'DELETE',
+        url: `/thing/${thingId}`,
+    }).then(
+        function (response) {
+            console.log('DELETE /thing/:id call successful');
+            console.log('response:', response);
+        }
+    ).catch(
+        function (error) {
+            console.log('DELETE /thing/:id call failed');
+            console.log('error:', error);
+        }
+    )
+}
+
+function makePutCall(thingId) {
+    axios({
+        method: 'PUT',
+        url: `/thing/${thingId}`,
+    }).then(
+        function (response) {
+            console.log('PUT /thing/:id call successful');
+            console.log('response:', response);
+        }
+    ).catch(
+        function (error) {
+            console.log('PUT /thing/:id call failed');
+            console.log('error:', error);
+        }
+    )
+}
+
--- server/routes/thing_router.js
+++ server/routes/thing_router.js
@@ -12,7 +12,36 @@ router.get('/', (req, res) => {
         .query(queryText)
         .then((dbResponse) => res.send(dbResponse.rows))
         .catch((dbErr) => {
-            console.log('Patient save failed: ', dbErr);
+            console.log('GET /thing failed: ', dbErr);
+            res.sendStatus(500);
+        });
+});
+
+router.get('/search', (req, res) => {
+    console.log('GET /thing?id= request received!');
+
+    let thingId = req.query.thingId;
+    let thingStatus = req.query.thingStatus;
+
+    console.log('thingId:', thingId);
+    console.log('thingStatus:', thingStatus);
+
+    const queryText = `
+        SELECT
+            *
+        FROM
+            "thing"
+        WHERE
+            uuid=$1
+        AND
+            thing_status=$2
+        `;
+
+    pool
+        .query(queryText, [thingId, thingStatus])
+        .then((dbResponse) => res.send(dbResponse.rows))
+        .catch((dbErr) => {
+            console.log('GET /thing/search with query failed: ', dbErr);
             res.sendStatus(500);
         });
 });
@@ -24,16 +53,64 @@ router.post('/', (req, res) => {
 
     const thingDescription = reqBody.thingDescription;
 
-    const queryText = `INSERT INTO "thing" (thing_description)
-    VALUES ($1) RETURNING uuid`;
+    const queryText = `
+        INSERT INTO "thing"
+            (thing_description)
+        VALUES
+            ($1)
+        RETURNING
+            uuid
+        `;
 
     pool
         .query(queryText, [thingDescription])
         .then((dbResponse) => res.status(201).send({ dbResponse: dbResponse.rows[0] }))
         .catch((dbErr) => {
-            console.log('Thing save failed: ', dbErr);
+            console.log('POST /thing failed: ', dbErr);
             res.sendStatus(500);
         });
+})
+
+router.delete('/:id', (req, res) => {
+    let reqId = req.params.id;
+    console.log('Delete request for id', reqId);
+
+    let sqlText = `
+        DELETE FROM "thing"
+        WHERE
+            uuid=$1
+        RETURNING
+            uuid;
+    `;
+
+    pool.query(sqlText, [reqId])
+        .then((dbResponse) => res.status(200).send({ dbResponse: dbResponse.rows[0] }))
+        .catch((dbErr) => {
+            console.log(`DELETE /thing failed.\nquery: \n${sqlText}\nfor id:\n${reqId}`, dbErr);
+            res.sendStatus(500);
+        })
+})
+
+router.put('/:id', (req, res) => {
+    let reqId = req.params.id;
+    console.log('Put request for id', reqId);
+
+    let sqlText = `
+        UPDATE "thing"
+        SET
+            thing_status = NOT thing_status
+        WHERE
+            uuid=$1
+        RETURNING
+            uuid, thing_status;
+    `;
+
+    pool.query(sqlText, [reqId])
+        .then((dbResponse) => res.status(200).send({ dbResponse: dbResponse.rows[0] }))
+        .catch((dbErr) => {
+            console.log(`PUT /thing failed.\nquery: \n${sqlText}\nfor id:\n${reqId}`, dbErr);
+            res.sendStatus(500);
+        })
 })
 
 module.exports = router;
